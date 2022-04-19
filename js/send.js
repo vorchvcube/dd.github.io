409e61098902a760dddd1c681aada799088aa673
vorch <vorchvcube@gmail.com>
2022-04-19 20:44:50 +0300

    main

--- js/send.js
+++ js/send.js
@@ -1,8 +1,6 @@
 
 $(function() {
-
     $( ".send" ).click(function() {
-        console.log($(".name").val())
         $.ajax({
             url: "./send.php", // Куда отправляем данные (обработчик)
             type: "POST", // Тип запроса
@@ -19,25 +17,25 @@ $(function() {
                     bgColor : 'rgb(255, 207, 0)',              // Background color for toast
                     textColor : '#eee',            // text color
                     allowToastClose : false,       // Show the close button or not
-                    hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
+                    hideAfter : 8000,              // `false` to make it sticky or time in miliseconds to hide after
                     stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
                     textAlign : 'left',            // Alignment of text i.e. left, right, center
-                    position : 'bottom-left'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                    position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
                 })
                 console.log(data)
 
             },
             error: function () {
                 $.toast({
-                    text : "Ошибка",
+                    text : "Ошибка, заявка не отправлена свяжитесь с нами любым способом",
                     showHideTransition : 'slide',  // It can be plain, fade or slide
                     bgColor : 'rgb(108,0,3)',              // Background color for toast
                     textColor : '#eee',            // text color
                     allowToastClose : false,       // Show the close button or not
-                    hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
+                    hideAfter : 8000,              // `false` to make it sticky or time in miliseconds to hide after
                     stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
                     textAlign : 'left',            // Alignment of text i.e. left, right, center
-                    position : 'bottom-left'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                    position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
                 })
             }
         });
