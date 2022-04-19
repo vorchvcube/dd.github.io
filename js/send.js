0b1b50904ac06db4ba143be1bc04d7dfeeef7d4d
vorch <vorchvcube@gmail.com>
2022-04-19 20:34:37 +0300

    main

--- js/send.js
+++ js/send.js
@@ -13,9 +13,32 @@ $(function() {
                 "phone": $(".phone").val() // Сообщение
             },
             success: function(data) {
-
+                $.toast({
+                    text : "Заявка успешно отправлена",
+                    showHideTransition : 'slide',  // It can be plain, fade or slide
+                    bgColor : 'rgb(255, 207, 0)',              // Background color for toast
+                    textColor : '#eee',            // text color
+                    allowToastClose : false,       // Show the close button or not
+                    hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
+                    stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                    textAlign : 'left',            // Alignment of text i.e. left, right, center
+                    position : 'bottom-left'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                })
                 console.log(data)
 
+            },
+            error: function () {
+                $.toast({
+                    text : "Ошибка",
+                    showHideTransition : 'slide',  // It can be plain, fade or slide
+                    bgColor : 'rgb(108,0,3)',              // Background color for toast
+                    textColor : '#eee',            // text color
+                    allowToastClose : false,       // Show the close button or not
+                    hideAfter : 5000,              // `false` to make it sticky or time in miliseconds to hide after
+                    stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                    textAlign : 'left',            // Alignment of text i.e. left, right, center
+                    position : 'bottom-left'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                })
             }
         });
     });
