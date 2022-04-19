f3e56a677c8b800fc03017942f724f6cae442682
vorch <vorchvcube@gmail.com>
2022-04-19 17:45:29 +0300

    main

--- js/send.js
+++ js/send.js
@@ -1,15 +1,16 @@
 
 $(function() {
-    console.log($("#name").val())
+
     $( ".send" ).click(function() {
+        console.log($(".name").val())
         $.ajax({
             url: "./send.php", // Куда отправляем данные (обработчик)
             type: "POST", // Тип запроса
             dataType: "json",
             data: {
-                "name": $("#Name").val(), // Имя
-                "email": $("#Email").val(), // E-mail
-                "message": $("#phone").val() // Сообщение
+                "name": $(".name").val(), // Имя
+                "email": $(".email").val(), // E-mail
+                "message": $(".phone").val() // Сообщение
             },
             success: function(data) {
 
