dd5723c65b347006ab1346992e08f80218d40a6d
aleks <vcube@yandex.ru>
2022-04-21 14:49:02 +0300

    main

--- js/send.js
+++ js/send.js
@@ -48,4 +48,104 @@ $(document).ready(function(){
             }
         });
     });
+
+    $( ".send1" ).click(function() {
+        console.log('asdasdasd')
+        console.log($(".name"))
+        $.ajax({
+            url: "./send.php", // Куда отправляем данные (обработчик)
+            type: "POST", // Тип запроса
+            dataType: "json",
+            data: {
+                "name": $(".name1").val(), // Имя
+                "email": $(".email1").val(), // E-mail
+                "phone": $(".phone1").val() // Сообщение
+            },
+            success: function(data) {
+
+                console.log(data)
+
+
+            },
+            error: function (data) {
+                console.log(data)
+                if (data.responseText === 'Отправлено!') {
+                    $.toast({
+                        text : "Заявка успешно отправлена",
+                        showHideTransition : 'slide',  // It can be plain, fade or slide
+                        bgColor : 'rgb(255, 207, 0)',              // Background color for toast
+                        textColor : '#eee',            // text color
+                        allowToastClose : false,       // Show the close button or not
+                        hideAfter : 8000,              // `false` to make it sticky or time in miliseconds to hide after
+                        stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                        textAlign : 'left',            // Alignment of text i.e. left, right, center
+                        position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                    })
+                } else {
+                    $.toast({
+                        text : "Ошибка, заявка не отправлена свяжитесь с нами любым способом",
+                        showHideTransition : 'slide',  // It can be plain, fade or slide
+                        bgColor : 'rgb(108,0,3)',              // Background color for toast
+                        textColor : '#eee',            // text color
+                        allowToastClose : false,       // Show the close button or not
+                        hideAfter : 8000,              // `false` to make it sticky or time in miliseconds to hide after
+                        stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                        textAlign : 'left',            // Alignment of text i.e. left, right, center
+                        position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                    })
+                }
+
+            }
+        });
+    });
+
+    $( ".send2" ).click(function() {
+        console.log('asdasdasd')
+        console.log($(".name"))
+        $.ajax({
+            url: "./send.php", // Куда отправляем данные (обработчик)
+            type: "POST", // Тип запроса
+            dataType: "json",
+            data: {
+                "name": $(".name2").val(), // Имя
+                "email": $(".email2").val(), // E-mail
+                "phone": $(".phone2").val() // Сообщение
+            },
+            success: function(data) {
+
+                console.log(data)
+
+
+            },
+            error: function (data) {
+                console.log(data)
+                if (data.responseText === 'Отправлено!') {
+                    $.toast({
+                        text : "Заявка успешно отправлена",
+                        showHideTransition : 'slide',  // It can be plain, fade or slide
+                        bgColor : 'rgb(255, 207, 0)',              // Background color for toast
+                        textColor : '#eee',            // text color
+                        allowToastClose : false,       // Show the close button or not
+                        hideAfter : 8000,              // `false` to make it sticky or time in miliseconds to hide after
+                        stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                        textAlign : 'left',            // Alignment of text i.e. left, right, center
+                        position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                    })
+                } else {
+                    $.toast({
+                        text : "Ошибка, заявка не отправлена свяжитесь с нами любым способом",
+                        showHideTransition : 'slide',  // It can be plain, fade or slide
+                        bgColor : 'rgb(108,0,3)',              // Background color for toast
+                        textColor : '#eee',            // text color
+                        allowToastClose : false,       // Show the close button or not
+                        hideAfter : 8000,              // `false` to make it sticky or time in miliseconds to hide after
+                        stack : 5,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                        textAlign : 'left',            // Alignment of text i.e. left, right, center
+                        position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                    })
+                }
+
+            }
+        });
+    });
 });
